7fdd98327051a0dfe476eb55d73c4f91b57f5fe0
vitalygl <vitalywashere@gmail.com>
2026-04-11 21:30:49 +0300

    Trie for logical operations (max xor, count of greater numbers after xor) added

---
+++ ForFreePrimitives/Trie.cs
@@ -0,0 +1,116 @@
+﻿namespace ForFreePrimitives
+{
+    public class BitTrie
+    {
+        private class Node
+        {
+            public Node[] children = new Node[2];
+            public int count = 0;
+        }
+
+        private readonly int maxLength = 8 * sizeof(long);
+        private readonly Node root = new Node();
+
+        public int Count { get => root.count; }
+
+        public BitTrie()
+        {
+        }
+
+        public BitTrie(int maxLength)
+        {
+            this.maxLength = maxLength;
+        }
+
+        private static int GetBit(long num, int index)
+        {
+            var bit = 1L << index;
+            return ((num & bit) == bit) ? 1 : 0;
+        }
+
+        public void Add(long num)
+        {
+            var node = root;
+            node.count += 1;
+            for (var i = maxLength - 1; i >= 0; i -= 1)
+            {
+                var bit = GetBit(num, i);
+                if (node.children[bit] == null)
+                    node.children[bit] = new Node();
+                node = node.children[bit];
+                node.count += 1;
+            }
+        }
+
+        public bool Remove(long num)
+        {
+            if (!Contains(num))
+                return false;
+            var node = root;
+            node.count -= 1;
+            for (var i = maxLength - 1; i >= 0; i -= 1)
+            {
+                var bit = GetBit(num, i);
+                node.children[bit].count -= 1;
+                if (node.children[bit].count <= 0)
+                {
+                    node.children[bit] = null;
+                    return true;
+                }
+                node = node.children[bit];
+            }
+            return true;
+        }
+
+        public bool Contains(long num)
+        {
+            var node = root;
+            for (var i = maxLength - 1; i >= 0; i -= 1)
+            {
+                var bit = GetBit(num, i);
+                if (node.children[bit] == null)
+                    return false;
+                node = node.children[bit];
+            }
+            return true;
+        }
+
+        public long GetMaxXor(long num)
+        {
+            if (Count <= 0)
+                return num;
+            var maxVal = 0L;
+            var node = root;
+            for (var i = maxLength - 1; i >= 0; i -= 1)
+            {
+                var bit = GetBit(num, i);
+                var target = bit ^ 1;
+                if (node.children[target] != null)
+                {
+                    maxVal += 1L << i;
+                    node = node.children[target];
+                    continue;
+                }
+                node = node.children[target ^ 1];
+            }
+            return maxVal;
+        }
+
+        public int GetGreaterXorCount(int n, int xorWith = 0)
+        {
+            var count = 0;
+            var node = root;
+            for (var i = maxLength - 1; i >= 0; i -= 1)
+            {
+                var bit = GetBit(n, i);
+                var xbit = bit ^ GetBit(xorWith, i);
+                if (bit == 0)
+                    count += node.children[xbit ^ 1]?.count ?? 0;
+                if (node.children[xbit] == null)
+                    break;
+                node = node.children[xbit];
+            }
+            return count;
+        }
+    }
+}
---
+++ ForFreePrimitivesTests/TrieTests.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using ForFreePrimitives;
+
+namespace ForFreePrimitivesTests
+{
+	[TestClass]
+	public class TrieTests
+	{
+
+		[TestMethod]
+		public void Tests()
+		{
+			ProcessDefinedTestCases();
+		}
+
+		private readonly int[][] trieTestCases = new int[][]
+		{
+			new int[] {1,2,3,4,5,1,2,3,4,5},
+		};
+
+		private readonly (int[] nums, int maxXor)[] maxXorTestCases = new (int[], int)[]
+		{
+			(new int[] {3,10,5,25,2,8}, 28),
+			(new int[] {14,70,53,83,49,91,36,80,92,51,66,70}, 127),
+		};
+
+		private readonly (int[] nums, int low, int high, int xorPairsInRangeCount)[] xorInRangeCountTestCases = new (int[], int, int, int)[]
+		{
+			(new int[] {1,4,2,7}, 2, 6, 6),
+			(new int[] {9,8,4,2,1}, 5, 14, 8),
+		};
+
+		private void ProcessDefinedTestCases()
+		{
+			foreach (var nums in trieTestCases)
+				ValidateTrie(nums);
+			foreach (var (nums, maxXor) in maxXorTestCases)
+				ValidateMaxXor(nums, maxXor);
+			foreach (var (nums, low, high, xorPairsInRangeCount) in xorInRangeCountTestCases)
+				ValidateXorInRangeCount(nums, low, high, xorPairsInRangeCount);
+		}
+
+		private void ValidateTrie(int[] nums)
+		{
+			var trie = new BitTrie(8 * sizeof(int));
+			var counter = new Dictionary<int, int>();
+			for (var i = 0; i < nums.Length; i += 1)
+			{
+				var num = nums[i];
+				trie.Add(num);
+				if (counter.TryGetValue(num, out var count))
+					counter[num] = count + 1;
+				else
+					counter.Add(num, 1);
+				Assert.IsTrue(trie.Count == (i + 1));
+				for (var j = 0; j < nums.Length; j += 1)
+					Assert.IsTrue(trie.Contains(nums[j]) == counter.ContainsKey(nums[j]));
+			}
+			for (var i = nums.Length - 1; i >= 0; i -= 1)
+			{
+				var num = nums[i];
+				Assert.IsTrue(trie.Remove(num));
+				Assert.IsTrue(trie.Count == i);
+				if (counter.TryGetValue(num, out var count))
+				{
+					if (count > 1)
+						counter[num] = count - 1;
+					else
+						counter.Remove(num);
+				}
+				var stillContains = counter.ContainsKey(num);
+				Assert.IsTrue(trie.Contains(num) == stillContains);
+				if (!stillContains)
+					Assert.IsFalse(trie.Remove(num));
+				for (var j = 0; j < nums.Length; j += 1)
+					Assert.IsTrue(trie.Contains(nums[j]) == counter.ContainsKey(nums[j]));
+			}				
+		}
+
+		private void ValidateMaxXor(int[] nums, int maxXor)
+		{
+			var trie = new BitTrie(8 * sizeof(int));
+			var max = 0;
+			foreach (var num in nums)
+			{
+				trie.Add(num);
+				max = Math.Max(max, (int)trie.GetMaxXor(num));
+			}
+			Assert.IsTrue(max == maxXor);
+		}
+
+		private void ValidateXorInRangeCount(int[] nums, int low, int high, int xorPairsInRangeCount)
+		{
+			var trie = new BitTrie(8 * sizeof(int));
+			var count = 0;
+			foreach (var num in nums)
+			{
+				count += trie.GetGreaterXorCount(low - 1, num);
+				count -= trie.GetGreaterXorCount(high, num);
+				trie.Add(num);
+			}
+			Assert.IsTrue(count == xorPairsInRangeCount);
+		}
+	}
+}
