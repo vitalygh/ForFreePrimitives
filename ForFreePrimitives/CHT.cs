5e02fa1c98c3aa92ce42ae0b2bac2d2c560ed338
vitalygl <vitalywashere@gmail.com>
2026-08-03 00:01:39 +0300

    Convex Hull Trick implementations (monotonic and Li Chao Tree) added

---
+++ ForFreePrimitives/CHT.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForFreePrimitives
+{
+    public class ConvexHullTrick
+    {
+        private class Line
+        {
+            public readonly long a = 0;
+            public readonly long b = 0;
+            public Line(long a, long b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+            public long Evaluate(long x)
+            {
+                return a * x + b;
+            }
+            public double IntersectionX(Line other)
+            {
+                return (double)(other.b - b) / (a - other.a);
+            }
+        }
+
+        private readonly List<Line> lines = new List<Line>();
+        private int head = 0;
+
+        public void Update(long a, long b)
+        {
+            var line = new Line(a, b);
+            while (lines.Count >= 2)
+            {
+                var last = lines[lines.Count - 1];
+                var prev = lines[lines.Count - 2];
+                if (prev.IntersectionX(line) > prev.IntersectionX(last))
+                    break;
+                lines.RemoveAt(lines.Count - 1);
+            }
+            lines.Add(line);
+        }
+
+        public long Query(long x)
+        {
+            head = Math.Min(head, lines.Count - 1);
+            while ((head < lines.Count - 1) && (lines[head + 1].Evaluate(x) < lines[head].Evaluate(x)))
+                head += 1;
+            return lines[head].Evaluate(x);
+        }
+
+        public long QueryBinary(long x)
+        {
+            var start = 0;
+            var end = lines.Count - 2;
+            while (start <= end)
+            {
+                var mid = start + (end - start) / 2;
+                if (lines[mid].IntersectionX(lines[mid + 1]) < x)
+                    start = mid + 1;
+                else
+                    end = mid - 1;
+            }
+            return lines[start].Evaluate(x);
+        }
+    }
+
+    public class LiChaoTree
+    {
+        private class Line
+        {
+            public readonly long a = 0;
+            public readonly long b = 0;
+            public Line(long a, long b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+            public long Evaluate(long x)
+            {
+                return a * x + b;
+            }
+            public double IntersectionX(Line other)
+            {
+                return (double)(other.b - b) / (a - other.a);
+            }
+        }
+
+        private readonly Line[] tree = null;
+
+        private readonly int minX = 0;
+        private readonly int maxX = 0;
+
+        public LiChaoTree(int minX, int maxX)
+        {
+            this.minX = minX;
+            this.maxX = maxX;
+            tree = new Line[4 * (maxX - minX + 1)];
+        }
+
+        public void Update(long a, long b)
+        {
+            Update(new Line(a, b), 0, minX, maxX);
+        }
+
+        public long Query(long x)
+        {
+            return Query(x, 0, minX, maxX);
+        }
+
+        private void Update(Line line, int index, int left, int right)
+        {
+            var node = tree[index];
+            if (node == null)
+            {
+                tree[index] = line;
+                return;
+            }
+            if ((line.Evaluate(left) >= node.Evaluate(left)) && (line.Evaluate(right) >= node.Evaluate(right)))
+                return;
+            var mid = left + (right - left) / 2;
+            var leftBetter = line.Evaluate(left) < node.Evaluate(left);
+            var midBetter = line.Evaluate(mid) < node.Evaluate(mid);
+            if (midBetter)
+                (line, tree[index]) = (tree[index], line);
+            if (left == right)
+                return;
+            if (leftBetter != midBetter)
+                Update(line, 2 * index + 1, left, mid);
+            else
+                Update(line, 2 * index + 2, mid + 1, right);
+        }
+
+        private long Query(long x, int index, int left, int right)
+        {
+            if ((index >= tree.Length) || (tree[index] == null))
+                return long.MaxValue;
+            var y = tree[index].Evaluate(x);
+            if (left == right)
+                return y;
+            var mid = left + (right - left) / 2;
+            if (x <= mid)
+                return Math.Min(y, Query(x, 2 * index + 1, left, mid));
+            return Math.Min(y, Query(x, 2 * index + 2, mid + 1, right));
+        }
+    }
+
+    public class LiChaoDynamicTree
+    {
+        private class Line
+        {
+            public readonly long a = 0;
+            public readonly long b = 0;
+            public Line(long a, long b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+            public long Evaluate(long x)
+            {
+                return a * x + b;
+            }
+            public double IntersectionX(Line other)
+            {
+                return (double)(other.b - b) / (a - other.a);
+            }
+        }
+
+        private class Node
+        {
+            public readonly long left = 0;
+            public readonly long right = 0;
+            public Line line = null;
+            public Node leftChild = null;
+            public Node rightChild = null;
+
+            public Node(Line line, long left, long right)
+            {
+                this.line = line;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        private readonly Node root = null;
+
+        public LiChaoDynamicTree(long minX, long maxX)
+        {
+            root = new Node(null, minX, maxX);
+        }
+
+        public void Update(long a, long b)
+        {
+            Update(root, new Line(a, b));
+        }
+
+        public long Query(long x)
+        {
+            return Query(root, x);
+        }
+
+        private void Update(Node node, Line line)
+        {
+            if (node.line == null)
+            {
+                node.line = line;
+                return;
+            }
+            var left = node.left;
+            var right = node.right;
+
+            if ((line.Evaluate(left) >= node.line.Evaluate(left)) && (line.Evaluate(right) >= node.line.Evaluate(right)))
+                return;
+
+            var mid = left + (right - left) / 2;
+            var leftBetter = line.Evaluate(left) < node.line.Evaluate(left);
+            var midBetter = line.Evaluate(mid) < node.line.Evaluate(mid);
+            if (midBetter)
+                (line, node.line) = (node.line, line);
+            if (left == right)
+                return;
+            if (leftBetter != midBetter)
+            {
+                if (node.leftChild == null)
+                    node.leftChild = new Node(null, left, mid);
+                Update(node.leftChild, line);
+            }
+            else
+            {
+                if (node.rightChild == null)
+                    node.rightChild = new Node(null, mid + 1, right);
+                Update(node.rightChild, line);
+            }
+        }
+
+        private long Query(Node node, long x)
+        {
+            if (node == null)
+                return long.MaxValue;
+            var y = (node.line == null) ? long.MaxValue : node.line.Evaluate(x);
+            var mid = node.left + (node.right - node.left) / 2;
+            if (x == mid)
+                return y;
+            if (x < mid)
+                return Math.Min(y, Query(node.leftChild, x));
+            return Math.Min(y, Query(node.rightChild, x));
+        }
+    }
+}
---
+++ ForFreePrimitivesTests/CHTTests.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ForFreePrimitives;
+
+namespace ForFreePrimitivesTests
+{
+	/// <summary>
+	/// Summary description for CHTTests
+	/// </summary>
+	[TestClass]
+	public class CHTTests
+	{
+		// Leetcode 3826
+		private static readonly (int[] nums, int k, long answer)[] minimalPartitionScoreTestcases = new[]
+		{
+			(new int[] { 45,71,34,89,63,83,91,91,73,95,89,96,66,28,90,79,1,77,75,61 }, 16, 63750L),
+            (new int[] { 35,73,100,16,24,39,70,46,62,54,77,71,83,78,91,99,89,54,60,55 }, 7, 117998L),
+            (new int[] { 13,30,25,65,74,48,90,34,13,80,7,62,28,89,50,81,31,59,52,40 }, 5, 95449L),
+            (new int[] { 24,52,39,54,46,51,58,54,39,98,74,48,23,78,98,73,87,55,83,30 }, 20, 39356L),
+        };
+
+
+		[TestMethod]
+		public void Tests()
+		{
+			ProcessDefinedTestCases();
+		}
+
+		private void ProcessDefinedTestCases()
+		{
+			foreach (var (nums, k, answer) in minimalPartitionScoreTestcases)
+				ValidateMPS(nums, k, answer);
+		}
+
+        public long MinPartitionScore(int[] nums, int k, Action<long, long> init, Action<long, long> update, Func<long, long> query)
+        {
+            var p = new long[nums.Length + 1];
+            for (var i = 0; i < nums.Length; i += 1)
+                p[i + 1] = p[i] + nums[i];
+            var dp = new long[2][];
+            for (var i = 0; i < dp.Length; i += 1)
+                dp[i] = new long[nums.Length + 1];
+            for (var i = 0; i < nums.Length; i += 1)
+                dp[0][i + 1] = p[i + 1] * (p[i + 1] + 1) / 2;
+            for (var d = 1; d < k; d += 1)
+            {
+                for (var i = 0; i < dp[1].Length; i += 1)
+                    dp[1][i] = long.MaxValue;
+                var minx = p[Math.Min(p.Length - 1, d + 1)];
+                var maxx = p[p.Length - 1];
+                init(minx, maxx);
+                for (var i = d; i < nums.Length; i += 1)
+                {
+                    var z = (p[i + 1] * p[i + 1] + p[i + 1]) / 2;
+                    var a = -p[i];
+                    var b = (p[i] * p[i] - p[i]) / 2 + dp[0][i];
+                    update(a, b);
+                    var min = query(p[i + 1]);
+                    dp[1][i + 1] = Math.Min(dp[1][i + 1], min + z);
+                }
+                (dp[0], dp[1]) = (dp[1], dp[0]);
+            }
+            return dp[0][nums.Length];
+        }
+
+        private void ValidateMPS(int[] nums, int k, long answer)
+		{
+            ConvexHullTrick cht = null;
+            var chtResult = MinPartitionScore(nums, k, (minx, maxx) =>
+            {
+                cht = new ConvexHullTrick();
+            }, 
+            (a, b) => cht.Update(a, b),
+            (x) => cht.Query(x));
+            LiChaoTree lct = null;
+            var lctResult = MinPartitionScore(nums, k, (minx, maxx) =>
+            {
+                lct = new LiChaoTree((int)minx, (int)maxx);
+            },
+            (a, b) => lct.Update(a, b),
+            (x) => lct.Query(x));
+            LiChaoDynamicTree lcdt = null;
+            var lcdtResult = MinPartitionScore(nums, k, (minx, maxx) =>
+            {
+                lcdt = new LiChaoDynamicTree(minx, maxx);
+            },
+            (a, b) => lcdt.Update(a, b),
+            (x) => lcdt.Query(x));
+            if ((chtResult != answer) || (lctResult != answer) || (lcdtResult != answer))
+                Assert.Fail($"new int[] {{{Tools.Dump(nums)}}}, {k}, {answer}L");
+        }
+	}
+}
